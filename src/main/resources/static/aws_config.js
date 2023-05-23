127de962c6ccb90ee554cc32ae57c5aa8978a9db
Esteban297 <estebancristancho9@gmail.com>
2023-05-22 20:45:34 -0500

    arreglo del bucket

--- src/main/resources/static/aws_config.js
+++ src/main/resources/static/aws_config.js
@@ -1,7 +1,7 @@
 AWS.config.update({
-    accessKeyId : 'ASIAQQ25ZEMYUKMOHHV4',
-    secretAccessKey : '2OoCMzTyIl1AGOqrqwkjyok36ACxwjhj1n7mbq',
-    sessionToken:'FwoGZXIvYXdzEEoaDIVJ76awOBpWgoAajCLYAcN6P8pb2EK/LGI9HGeMMNFVkeqkbY2HbOZxbr4Jx8R6IlUcml/iTnzgMX1ubbQGHFbtHtd1SbypzyxBvTTvHieYIcHriKd/4aV79SQEcLKCfAKcNs0aFfh2tfJEyg1KRisjJO7PmWDw5lSuhvKsJdhEVfOa92tRv2+NAc2cV2DHd9r/zJY0ma8XxWLOzjzbSsgVWYGfG60vTHIiFpAYyf4gL8LvaxnWVShiwjN8+u8JgnjTNmGFrQBrd9JT121KmdmpV32sczWiNyrw4vGV0CBBd9cT3b4qwyjXppmjBjIt8HZlhVePOLrmbO2WmGzafyd9+WdIQczvtPioxZayVDttvGgWk+vOz3rNSVmF'
+    accessKeyId : 'ASIAYF6OY2D6QDVI3LHF',
+    secretAccessKey : 'FLCKqqfxjgeFFRKTS8DfKP4dlhjTPoc21yQrEtoT',
+    sessionToken:'FwoGZXIvYXdzEKv//////////wEaDLXmnx+atGlSFLA0PCLZAV4blrQxBAef2Mm4WXUwc0Ia5YuRMjYHhUWpidYELrLDaUqyDSpbVDxPX/0MCxHnwskVxmO0yoxtBLbEuiZgkk0Pv50plQpxKL586fudBEkFPBrJ5QkX8mZ0dWQqkZIek0IE8j99RQjnGHhM08/JeyqSeXSH1x03QhrRD04D/FkAAlZ4H5M+E2Hy6g8n3BjxUXWu/+uilJMUbOJ1Zf3Z5eiZcx9dy+uIc56AMg9EtQinovt1dtXW1fNuHAH1hFG/x2oyoj584aOfcfWk/Di4e2rgOpZbG6nAZJIowMuuowYyLckk3/j6Q6Zr1M/w86cdW3WSuGQp0zH4vN1CVVbAeSq+dWrwm9lilFiZqL/K1g=='
 });
 AWS.config.region = 'us-east-1';
 
